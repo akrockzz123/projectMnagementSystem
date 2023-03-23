f6abe21152dd9508a75ad948889902da68c74f75
aniket <aniketkumar1601@gmail.com>
2023-03-23 11:09:29 +0530

    newRep

--- backend/sql/createDB.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS pmd;
-use pmd;
-CREATE TABLE IF NOT EXISTS user(
-    user_id int PRIMARY KEY AUTO_INCREMENT,
-    username varchar(50) NOT NULL,
-    email varchar(50) NOT NULL,
-    role ENUM("Admin","User") NOT NULL DEFAULT "User",
-    status ENUM("Active","Inactive") NOT NULL DEFAULT "Active"
-);
